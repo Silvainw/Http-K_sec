f367ce05506f09de98132906a4db8d77d95977ea
Silvainw <43095278+Silvainw@users.noreply.github.com>
2020-10-09 22:05:48 +0200

    http

---
+++ Index.php
@@ -0,0 +1,16 @@
+leandervandaag om 14:55
+<form name="inloggen" action="" method="POST">
+    <input type="text" name="email" value="iemand@gmail.com" />
+    <input type="password" name="password" value="wachtwoord" />
+    <input type="submit" name="submit" value="Inloggen">
+</form>
+
+<?php
+    if (isset($_POST['submit'])) {
+        echo "----------Backend----------";
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        echo "<br>e-mail: $email";
+        echo "<br>password: $password";
+    }
+?>
